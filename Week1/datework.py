4e86f96d65b51760387544be7126818c2f35798f
Osasere Ikponmwosa <edenosas1@gmail.com>
2024-11-13 14:32:50 +0100

    3500

---
+++ Week1/datework.py
@@ -0,0 +1,8 @@
+# birthday = input('enter your birthday dd/mm/yy ')
+# birthdate = datetime.strptime(birthday, '%d/%m/%Y')
+#
+# print(birthdate.year)
+
+day = datetime.now()
+
+print(datetime.weekday(day))
